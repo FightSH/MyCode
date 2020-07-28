8cb117d18d5e723cfaacc3b2e25a363a42425033
北洛 <shenhao189@foxmail.com>
2020-07-28 20:19:12 +0800

    小修改

--- designpattern/src/main/java/org/example/iteratorpattern/Client.java
+++ designpattern/src/main/java/org/example/iteratorpattern/Client.java
@@ -10,12 +10,12 @@ public class Client {
         handler1.nextHandler = handler2;
         handler2.nextHandler = handler3;
 
-        //构造三个请求者对象
+
         AbstractRequest request1 = new RequestOne("Request1");
         AbstractRequest request2 = new RequestTwo("Request2");
         AbstractRequest request3 = new RequestThree("Request3");
 
-        //总是从链式的首端发起请求
+        //首端请求
         handler1.handleRequest(request1);
         handler1.handleRequest(request2);
         handler1.handleRequest(request3);
