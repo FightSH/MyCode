8e1a1e59904eb09fca7d1b066b5245f48a358feb
hao <shenhao189@foxmail.com>
2021-02-25 18:04:06 +0800

    随便写写

--- multithreading/src/main/java/org/example/aqs/CountDownLatchDemo.java
+++ multithreading/src/main/java/org/example/aqs/CountDownLatchDemo.java
@@ -7,46 +7,34 @@ public class CountDownLatchDemo {
 
     public static void main(String[] args) throws InterruptedException {
 
-         CountDownLatch downLatch1 = new CountDownLatch(3);
-         CountDownLatch downLatch2 = new CountDownLatch(1);
+        CountDownLatch downLatch1 = new CountDownLatch(3);
+        CountDownLatch downLatch2 = new CountDownLatch(1);
 
 
         for (int i = 0; i < 3; i++) {
-            new Thread(new CountDownLatchDemo.Worker(downLatch2, downLatch1)).start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        downLatch2.await();
+                        System.out.println(Thread.currentThread().getName() + " id do something...");
+                        downLatch1.countDown();
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         }
 
-        System.out.println(Thread.currentThread().getName() + " is ready" );
+        System.out.println(Thread.currentThread().getName() + " is ready");
         downLatch2.countDown();
 
         downLatch1.await();
 
     }
 
-    public static class Worker implements Runnable {
 
-        private final CountDownLatch countDownLatch;
-        private final CountDownLatch countDownLatch2;
-
-        public Worker(CountDownLatch countDownLatch, CountDownLatch countDownLatch2) {
-            this.countDownLatch = countDownLatch;
-            this.countDownLatch2 = countDownLatch2;
-        }
-
-        @Override
-        public void run() {
-
-            try {
-                countDownLatch.await();
-                System.out.println(Thread.currentThread().getName() + " id do something...");
-                countDownLatch2.countDown();
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-
-        }
-    }
 
 
 }
