553a58552bfd715c304626cc219b4b341a303b8e
hao <email>
2021-04-13 11:32:33 +0800

    code

---
+++ algorithm/src/main/java/org/example/leetcode/tree/MinDiffInBST.java
@@ -0,0 +1,38 @@
+package org.example.leetcode.tree;
+
+
+import org.example.datastructure.tree.TreeNode;
+
+public class MinDiffInBST {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    int pre;
+    int ans;
+
+    public int minDiffInBST(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        pre = -1;
+        dfs(root);
+        return ans;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        if (pre != -1) {
+            ans = Math.min(ans, root.val - pre);
+        }
+        pre = root.val;
+        dfs(root.right);
+    }
+
+
+
+}
