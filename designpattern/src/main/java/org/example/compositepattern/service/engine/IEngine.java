dc6c9afa11d5ab3797d19b2131bc8fed2130d52a
shen <shenhao@iyunbao.com>
2021-08-27 17:12:21 +0800

    CompositePattern

--- designpattern/src/main/java/org/example/compositepattern/service/engine/IEngine.java
+++ designpattern/src/main/java/org/example/compositepattern/service/engine/IEngine.java
@@ -1,4 +1,15 @@
 package org.example.compositepattern.service.engine;
 
+import org.example.compositepattern.model.aggreates.TreeRich;
+import org.example.compositepattern.model.vo.EngineResult;
+
+import java.util.Map;
+
+/**
+ * 决策引擎接口
+ */
 public interface IEngine {
+    // 统一的处理操作，方便后续扩展
+    EngineResult process(final Long treeId, final String userId, TreeRich treeRich, final Map<String, String> decisionMatter);
+
 }
