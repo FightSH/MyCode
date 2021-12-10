ff8c66f3ad825f26bbbb9f60fc6e53053ee8a824
shen <shenhao@iyunbao.com>
2021-12-10 18:56:59 +0800

    旋转数组

--- algorithm/src/main/java/org/example/basicfirst/Rotate.java
+++ algorithm/src/main/java/org/example/basicfirst/Rotate.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 
 public class Rotate {
     public static void main(String[] args) {
-        int[] nums = {-1,-100,3,99};
-        int k = 2;
-        rotateOne(nums, k);
+        int[] nums = {-1, -100, 3, 99};
+        int k = 1;
+        rotateTwo(nums, k);
         System.out.println(Arrays.toString(nums));
     }
-    // 构建双倍长数组
+
+    // 构建双倍长数组 缺点占空间。
     public static void rotateOne(int[] nums, int k) {
         int length = nums.length;
         if (k == 0 || k % length == 0) {
@@ -28,6 +29,21 @@ public class Rotate {
     }
 
     public static void rotateTwo(int[] nums, int k) {
+        int length = nums.length;
+        if (k == 0 || k % length == 0) {
+            return;
+        }
+        if (k > length) {
+            k = k % length;
+        }
+        int a = 0;
+        int b = 0;
+        int temp = 0;
+        for (int i = 0; i < length; i += k) {
+            System.out.print(nums[i]);
+            System.out.print("-");
+            System.out.println(nums[(i + k) % k]);
+        }
 
 
     }
