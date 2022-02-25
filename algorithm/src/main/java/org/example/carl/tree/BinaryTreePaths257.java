00b40851fb2f1f588b609d87b3d9c52d8fa1d1a1
shen <shenhao@iyunbao.com>
2022-02-25 18:51:50 +0800

    tree path

--- algorithm/src/main/java/org/example/carl/tree/BinaryTreePaths257.java
+++ algorithm/src/main/java/org/example/carl/tree/BinaryTreePaths257.java
@@ -4,6 +4,7 @@ import org.example.datastructure.tree.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePaths257 {
 
@@ -18,11 +19,9 @@ public class BinaryTreePaths257 {
         return res;
 
     }
-
+    // 递归：确定递归的参数
     static void traversal(TreeNode cur, List<Integer> path, List<String> result) {
-
         path.add(cur.val);
-
         // 说明找到叶子节点
         // 下面为终止除了逻辑
         if (cur.right == null && cur.left == null) {
@@ -39,7 +38,7 @@ public class BinaryTreePaths257 {
         // 单层递归处理逻辑
         // 前序遍历，先处理中间节点
         // 递归完之后，需要回溯，因为 path 需要删节点
-
+        // 回溯与递归是一一对应的，有一个递归就要有一个回溯
         if (cur.left != null) {
             traversal(cur.left, path, result);
             path.remove(path.size() - 1);
@@ -48,8 +47,42 @@ public class BinaryTreePaths257 {
             traversal(cur.right, path, result);
             path.remove(path.size() - 1);
         }
-        // 回溯与递归是一一对应的，有一个递归就要有一个回溯
 
+    }
+    // 前序遍历迭代节点
+    static  List<String> binaryTreePathsTwo(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        // 节点和路径同时入栈
+        Stack<Object> stack = new Stack<>();
+
+        stack.push(root);
+        stack.push(root.val + "");
+
+        while (!stack.isEmpty()) {
+            String path = (String) stack.pop();
+            TreeNode node = (TreeNode) stack.pop();
+            // 找到叶子节点
+            if (node.right == null && node.left == null) {
+                res.add(path);
+            }
+
+            // 向右找
+            if (node.right != null) {
+                stack.push(node.right);
+                stack.push(path + "->" + node.right.val);
+            }
+
+            if (node.left != null) {
+                stack.push(node.left);
+                stack.push(path + "->" + node.left.val);
+            }
+
+
+        }
+        return res;
     }
 
 }
