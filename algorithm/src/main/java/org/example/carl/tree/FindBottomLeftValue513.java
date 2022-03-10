e8be5fc54ab1c8daf9d8189cd21c1b57bb917317
shen <shenhao@iyunbao.com>
2022-03-10 17:40:56 +0800

    层序遍历

---
+++ algorithm/src/main/java/org/example/carl/tree/FindBottomLeftValue513.java
@@ -0,0 +1,38 @@
+package org.example.carl.tree;
+
+import org.example.datastructure.tree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FindBottomLeftValue513 {
+
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == 0) {
+                    res = node.val;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+
+            }
+        }
+        return res;
+
+
+
+
+    }
+
+}
